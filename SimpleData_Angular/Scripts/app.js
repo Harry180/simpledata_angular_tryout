6311d45fe2438135ca01e9fe559a2fb270a39b2f
harry <lukasz.charubin1989@gmail.com>
2015-01-03 23:02:37 +0100

    Added Proper controller, view and configured angular to work properly

---
+++ SimpleData_Angular/Scripts/app.js
@@ -0,0 +1,4 @@
+﻿var app = angular.module('karolSeals', []);
+app.controller("SealController", function () {
+	this.Seals = [{ FirstName: "Ola", LastName: "Jakas", Age: 22, Photo: null }, { FirstName: "Kasia", LastName: "Jakas", Age: 13, Photo: null }];
+});
