fce5135136acc7b71f7550a2f0eea9b993afc7eb
harry <lukasz.charubin1989@gmail.com>
2015-01-05 23:20:13 +0100

    First step for edit functionality angular part

--- SimpleData_Angular/Scripts/app.js
+++ SimpleData_Angular/Scripts/app.js
@@ -1,9 +1,37 @@
-﻿var app = angular.module('karolSeals', []);
-app.controller("SealController", ['$http', function ($http) {
+﻿
+var app = angular.module('karolSeals', []);
+app.controller("SealController", ['$scope', '$http', function ($scope, $http) {
 	var self = this;
 	self.seals = [];
 
 	$http.get('http://simpledata.com/Seal/GetSeals').success(function (data) {
 		self.seals = data;
 	});
+	//TODO: Is this good approach for passing value: google it !
+	self.editSeal = function (seal) {
+		$scope.$broadcast('sealEdit', seal);
+	};
 }]);
+
+app.controller('DetailsController', ['$scope', '$http', function ($scope, $http) {
+	var self = this;
+	self.sealModel = {};
+
+	$scope.$on('sealEdit', function (response) {
+		console.log(response);
+	});
+
+	self.submitSeal = function () {
+
+	};
+
+}]);
+
+$(function () {
+	$('#sealFormWindow').dialog({
+		autoOpen: false,
+		height: 300,
+		width: 350,
+		modal: true
+	});
+});
